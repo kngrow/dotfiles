bf1cdff6f1ebd958f72209b95025a0aed228b766
pinon <ymc9.knit.p23@gmail.com>
2017-02-16 01:22:58 +0900

    init.sh 更新？（動くかわかんない

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 
 x=`pwd`
 echo 'this directory: '${x}
+mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+ git clone https://github.com/Shougo/dein.vim.git \
+    ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
 echo 'link .vimrc'
 ln -s ${x}/.vimrc $HOME/.vimrc
